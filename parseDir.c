bd71db662a8b7addd2d46009d9c2a1ecac682965
Adriaan Pelzer <adriaan.pelzer@reachplc.com>
2018-08-12 09:37:13 +0000

    Create get_file_content

--- parseDir.c
+++ parseDir.c
@@ -26,6 +26,16 @@ void file_list_free ( file_list_t *file_list ) {
     }
 }
 
+void fileCtx_free ( fileCtx_t *fileCtx ) {
+    if ( fileCtx ) {
+        if ( fileCtx->fn )
+            free ( fileCtx->fn );
+        if ( fileCtx->fp )
+            fclose ( fileCtx->fp );
+        free ( fileCtx );
+    }
+}
+
 file_list_t *insert_into_file_list ( file_list_t *file_list, struct stat stat_buf, const char *name ) {
     file_list_t *rc = file_list, *file_entry = NULL, *prev = NULL, *next = NULL;
 
@@ -90,10 +100,15 @@ over:
     return rc;
 }
 
-FILE *open_lock_oldest_unlocked_file ( const char *folder ) {
-    FILE *rc = NULL, *_rc = NULL;
+fileCtx_t *open_lock_oldest_unlocked_file ( const char *folder ) {
+    fileCtx_t *rc = NULL, *_rc = NULL;
     file_list_t *ordered_file_list = NULL, *file_entry = NULL;
     char found = 0;
+
+    if ( ( _rc = malloc ( sizeof ( fileCtx_t ) ) ) == NULL )
+        goto over;
+
+    memset ( _rc, 0, sizeof ( fileCtx_t ) );
 
     if ( ( ordered_file_list = get_ordered_file_list ( folder ) ) == NULL )
         goto over;
@@ -101,10 +116,13 @@ FILE *open_lock_oldest_unlocked_file ( const char *folder ) {
     file_entry = ordered_file_list;
 
     while ( file_entry ) {
-        if ( ( _rc = fopen ( file_entry->name, "r" ) ) == NULL )
+        if ( ( _rc->fp = fopen ( file_entry->name, "r" ) ) == NULL )
             goto over;
 
-        if ( flock ( fileno ( _rc ), LOCK_EX | LOCK_NB ) == 0 ) {
+        if ( flock ( fileno ( _rc->fp ), LOCK_EX | LOCK_NB ) == 0 ) {
+            if ( ( _rc->fn = strndup ( file_entry->name, strlen ( file_entry->name ) ) ) == NULL )
+                goto over;
+
             found = 1;
             break;
         }
@@ -118,7 +136,7 @@ FILE *open_lock_oldest_unlocked_file ( const char *folder ) {
     rc = _rc;
 over:
     if ( rc != _rc && _rc != NULL )
-        fclose ( _rc );
+        fileCtx_free ( _rc );
 
     if ( ordered_file_list )
         file_list_free ( ordered_file_list );
@@ -126,36 +144,37 @@ over:
     return rc;
 }
 
-int close_unlock_file ( FILE *fp ) {
-    if ( flock ( fileno ( fp ), LOCK_UN ) != 0 )
+char *get_file_content ( fileCtx_t *fileCtx ) {
+    char *rc = NULL, *_rc = NULL;
+    size_t bytes_read = 0;
+
+    while ( ! feof ( fileCtx->fp ) ) {
+        if ( ( _rc = realloc ( _rc, bytes_read + 1024 ) ) == NULL )
+            goto over;
+        bytes_read = bytes_read + fread ( _rc + bytes_read, 1, 1024, fp );    
+    }
+
+    rc = _rc;
+over:
+    if ( rc != _rc && _rc != NULL )
+        free ( _rc );
+
+    return rc;
+}
+
+int close_unlock_file ( fileCtx_t *fileCtx ) {
+    if ( flock ( fileno ( fileCtx->fp ), LOCK_UN | LOCK_NB ) != 0 )
         return EXIT_FAILURE;
-    if ( fclose ( fp ) != 0 )
+    if ( fclose ( fileCtx->fp ) != 0 )
         return EXIT_FAILURE;
     return EXIT_SUCCESS;
 }
 
-int get_filename ( FILE *fp, char **filename ) {
-    char path[256];
-
-    snprintf ( path, 256, "/proc/self/fd/%d", fileno ( fp ) );
-    memset ( *filename, 0, 256 );
-
-    if ( readlink ( path, *filename, 256 ) < 0 )
+int close_unlink_file ( fileCtx_t *fileCtx ) {
+    if ( fclose ( fileCtx->fp ) != 0 )
         return EXIT_FAILURE;
 
-    return EXIT_SUCCESS
-}
-
-int close_unlink_file ( FILE *fp ) {
-    char path[256], filename[256];
-
-    if ( get_filename ( fp, &filename ) != EXIT_SUCCESS )
-        return EXIT_FAILURE;
-
-    if ( fclose ( fp ) != 0 )
-        return EXIT_FAILURE;
-
-    if ( unlink ( filename ) != 0 )
+    if ( unlink ( fileCtx->fn ) != 0 )
         return EXIT_FAILURE;
 
     return EXIT_SUCCESS;
--- parseDir.h
+++ parseDir.h
@@ -1,10 +1,16 @@
+#include <stdio.h>
+
 #ifndef _PARSEDIR_H_
 #define _PARSEDIR_H_
 
-#include <stdio.h>
+typedef struct fileCtx {
+    char *fn;
+    FILE *fp;
+} fileCtx_t;
 
-FILE *open_lock_oldest_unlocked_file ( const char *folder );
-int close_unlock_file ( FILE *fd );
-int close_unlink_file ( FILE *fd );
+fileCtx_t *open_lock_oldest_unlocked_file ( const char *folder );
+char *get_file_content ( fileCtx_t *fileCtx );
+int close_unlock_file ( fileCtx_t *fileCtx );
+int close_unlink_file ( fileCtx_t *fileCtx );
 
 #endif
